462dfcb198bffea653792169696282dc4d9788a0
Margarita Samsonova <ritiksamsonova@yandex.ru>
2020-03-23 20:20:16 +0300

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TodosComponent } from './todos/todos.component';
+import { TodoFormComponent } from './todo-form/todo-form.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodosComponent,
+    TodoFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/todos/todos.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import {TodosService} from '../shared/todos.service';
+
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.scss']
+})
+export class TodosComponent implements OnInit {
+
+  constructor(public todosService: TodosService) { }
+
+  ngOnInit() {
+  }
+
+  onChange(id: number) {
+    this.todosService.onToggle(id)
+  }
+
+  removeTodo(id:number) {
+    this.todosService.removeTodo(id)
+  }
+
+  editTask(id: number) {
+    console.log(id)
+  }
+
+}
